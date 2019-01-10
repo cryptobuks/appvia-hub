76dedfd599dc61561f288482821585ba599e9540
Jits <dev@jits.co.uk>
2019-01-10 16:18:27 +0000

    Web: Clean up generated code + more VSCode workspace settings

--- appvia-hub-web/e2e/src/app.e2e-spec.ts
+++ appvia-hub-web/e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to appvia-hub-web!');
+    expect(page.getTitleText()).toEqual('Welcome to the Appvia Hub!');
   });
 });
