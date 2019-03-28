3a9e17a70039b182ae2df69518dbefbd4309d554
Jits <dev@jits.co.uk>
2019-03-28 15:05:46 +0000

    Upgrade to latest Webpacker v4
    This now comes with Webpack v4, which is fasterer and betterer.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,15 @@
 const { environment } = require('@rails/webpacker');
 const webpack = require('webpack');
-const vue = require('./loaders/vue');
-const erb = require('./loaders/erb');
 
-environment.loaders.append('vue', vue);
-environment.loaders.append('erb', erb);
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+const erb = require('./loaders/erb');
+const vue = require('./loaders/vue');
+
+environment.loaders.prepend('erb', erb);
+
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin());
+environment.loaders.prepend('vue', vue);
 
 environment.plugins.append(
   'Provide',
