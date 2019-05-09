cryptobuks/appvia-hub

commit 32fbc69bfdc03218d8833df1f0e11988029eefb3

Global Branch Protection
- adds the ability to enable global branch protection of master, disabling push force, deletion and requiring reviews
- @note: due to limitations on the github api, wildcards is not supported thus the branches must before enabling protection
  on them. This forces us to enable the 'auto_init' to create a master branch on our behalf.

app/agents/git_hub_agent.rb
@@ -1,23 +1,47 @@
 class GitHubAgent
-  def initialize(app_id:, app_private_key:, app_installation_id:, org:)
+  def initialize(app_id:, app_private_key:, app_installation_id:, org:, app_require_protection:)
     @app_id = app_id
     @app_private_key = OpenSSL::PKey::RSA.new(app_private_key.gsub('\n', "\n"))
     @app_installation_id = app_installation_id
     @org = org
+    @app_require_protection = app_require_protection.match?("true")
 
     setup_client
   end
 
   def create_repository(name, private: false)
-    app_installation_client.create_repository(
-      name,
-      private: private,
-      organization: @org
-    )
+    unless app_installation_client.repository?(name, organization: @org)
+      app_installation_client.create_repository(
+        name,
+        organization: @org,
+        private: private,
+        auto_init: @app_require_protection,
+      )
+    end
+
+    # https://github.community/t5/GitHub-API-Development-and/REST-API-v3-wildcard-branch-protection/td-p/14547
+    if @app_require_protection
+      full_name = "#{@org}/#{name}"
+      app_installation_client.protect_branch(full_name, "master", {
+        enforce_admins: true,
+        required_status_checks: {
+          contexts: [],
+          strict: true,
+        },
+        required_pull_request_reviews: {
+          dismiss_stale_reviews: true,
+          require_code_owner_reviews: true,
+        },
+      })
+    end
   end
 
   def delete_repository(full_name)
-    app_installation_client.delete_repository full_name
+    slug = full_name.split("/").last
+
+    if app_installation_client.repository?(slug, organization: @org)
+     app_installation_client.delete_repository(full_name)
+    end
   end
 
   private

.gitignore
@@ -4,8 +4,12 @@
 # or operating system, you probably want to add a global ignore instead:
 #   git config --global core.excludesfile '~/.gitignore_global'
 
+# Ignore vim files
+*.swp
+
 # Ignore bundler config.
 /.bundle
+vendor/
 
 # Ignore all logfiles and tempfiles.
 /log/*

app/workers/resources/base_worker.rb
@@ -8,6 +8,7 @@ module Resources
           app_id: config['app_id'],
           app_private_key: config['app_private_key'],
           app_installation_id: config['app_installation_id'],
+          app_require_protection: config['app_require_protection'],
           org: config['org']
         )
       end,

config/providers.yml
@@ -19,11 +19,16 @@
         title: Your GitHub App installation on your org
         description: "The specific installation of your GitHub App within your GitHub organisation."
         type: string
+      app_require_protection:
+        title: Enforce Branch Protection (Master Branch)
+        description: "Ensures protection against push-force, deletes and requires reviewes on master branch. (true|false)"
+        type: string
     required:
-      - org
       - app_id
-      - app_private_key
       - app_installation_id
+      - app_private_key
+      - app_require_protection
+      - org
 
 - id: quay
   name: Quay

commit 8c4c8d441c2ca3f71c2500ffe0cba014f5443a60

Fix `delete_repository` in the GitHub Agent
Broken in https://github.com/appvia/appvia-hub/commit/32fbc69bfdc03218d8833df1f0e11988029eefb3#diff-3bedcc0ee62e37565625cc97d128d128R42

app/agents/git_hub_agent.rb
@@ -36,8 +36,7 @@ class GitHubAgent
   end
 
   def delete_repository(full_name)
-    slug = full_name.split('/').last
-    return unless app_installation_client.repository?(slug, organization: @org)
+    return unless app_installation_client.repository? full_name
 
     app_installation_client.delete_repository(full_name)
   end