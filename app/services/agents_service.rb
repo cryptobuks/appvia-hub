768eaa90c79debefd0db80ba1c43d4fcff9e08ae
Daniel Whatmuff <danielwhatmuff@gmail.com>
2019-06-26 15:38:33 +0100

    adding missing loki_data_source_name param

--- app/services/agents_service.rb
+++ app/services/agents_service.rb
@@ -51,7 +51,8 @@ module AgentsService
       end,
       'loki' => lambda do |config|
         LokiAgent.new(
-          grafana_url: config['grafana_url']
+          grafana_url: config['grafana_url'],
+          loki_data_source_name: config['loki_data_source_name']
         )
       end
     }.freeze
